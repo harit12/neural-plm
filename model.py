a6d2d5ae97926d7abd7d6577b7dc1ca514c175b8
harit12 <hmetla1245@gmail.com>
2021-01-07 19:51:13 -0500

    Changed to speed up batch creation

--- model.py
+++ model.py
@@ -12,6 +12,9 @@ class DataSet:
         self.vocab = DataSet.gen_vocab(data)
     def split_data(self, training, val, test):
         """
+        args: training(type: float, expl: percentage of dataset for training), val(type: float, expl: percentage of dataset for validation), test(type: float, expl: percentage of dataset for testing)
+        Splits dataset into 3 sets using the arguements
+        return: self.data_splitted(type:instance variable(dict), expl: dictionary of the training, validation, and testing set)
         """
         size = len(self.data)
         train_data = self.data[0:int(size*training)]
@@ -24,24 +27,35 @@ class DataSet:
             }
     def gen_vocab(data):
         """
+        STATIC METHOD
+        args: data(type: list, expl: dataset to generate vocab from)
+        Generates vocabulary from dataset
+        return: res(type: list, expl: list of vocab)
         """
         data = [word for sentence in nltk.corpus.brown.sents() for word in sentence]
         res = list(set(data))
-        return res
+        res_2 = {word:x for x,word in enumerate(res)}
     def w2idx(self, word):
         """
+        args: data(type: String, expl: word to get index in vocab of)
+        Gets index of word in vocab
+        return: res(type: int, expl: index of word in vocab)
         """
-        vocab = self.vocab
-        res = vocab.index(word)
+        vocab = self.vocab_eff
+        res = vocab[word]
         return res
     def idx2w(self, index):
         """
+        args: index(type: int, expl: index of word in vocab)
+        Gets word from index 
+        return res(type: String, expl: word from index)
         """
         vocab = self.vocab
         res = vocab[index]
         return res
     def sample2idx(self, sample):
         """
+        
         """
         return [self.w2idx(word) for word in sample]
     def idx2sample(self, idx):
@@ -55,32 +69,33 @@ class DataSet:
         data = self.data_splitted[split]
         random = np.random.randint(0, len(data)-n)
         sample = (data[random:random+n-1], data[random+n])
-        sample = (torch.LongTensor(self.sample2idx(sample[0])), torch.LongTensor([self.w2idx(sample[1])]))
+        #print(datetime.datetime.now(), 'create sample')
+        sample = (np.array(self.sample2idx(sample[0])), np.array([self.w2idx(sample[1])]))
         return sample
 
     def gen_batch(self, n,batch_size, split = 'training'):
         """
         """
+        """
+        """
         data = self.data_splitted[split]
         batch = [self.gen_sample(n) for x in range(batch_size)]
-        xs = list()
-        ys = list()
-        for sample in batch:
-            #print(sample)
-            x,y = sample
-            xs.append(x)
-            ys.append(y)
-        x_tens = torch.stack(xs)
+        #print(datetime.datetime.now(), 'create batch')
+        xs = [i[0] for i in batch]
+        ys = [i[1] for i in batch]
+        #print(datetime.datetime.now(), 'list time')
+        x_tens = np.stack(xs, axis=0)
         #print(x_tens)
-        y_tens = torch.stack(ys)
-        batch = (x_tens, y_tens)
+        y_tens = np.stack(ys, axis=0)
+        #print(datetime.datetime.now(), 'stack time')
+        batch = (torch.from_numpy(x_tens), torch.from_numpy(y_tens))
+        #print(datetime.datetime.now(), 'conv time')
+        return batch
         return batch
 
 class Model(nn.Module):
 
     def __init__(self, data, m, n):
-        """
-        """
         super(Model, self).__init__()
         n = n-1
         self.embedding = nn.Embedding(len(data.vocab), m)
@@ -111,7 +126,7 @@ def train(data,n = 3, m = 30, batch_size = 128, lr = .01, epochs = 2500, momentu
     data_count = len(data.data_splitted['training'])
     #print(int(data_count/batch_size/5))
     for epoch in range(epochs):
-        for _ in range(int(data_count/batch_size)-n):
+        for i in range(int(data_count/batch_size)-n):
             x, y = data.gen_batch(n, batch_size)
             y = y.squeeze(1)
             #print(x,y)
@@ -121,6 +136,8 @@ def train(data,n = 3, m = 30, batch_size = 128, lr = .01, epochs = 2500, momentu
             loss.backward()
             optimizer.step()
             #print(i)
+            if(i%100==1):
+              print(i, datetime.datetime.now(), '100 iters')
         if loss<.01:
             break
         print('Epoch: {}\nLoss:{}'.format(epoch, loss))
